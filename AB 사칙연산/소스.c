83b4ff54e126bfda7c82d79da02c8615615f4c94
KimYongHwan97 <koh5594@Naver.com>
2020-10-06 10:44:03 +0900

    AB사칙연산

---
+++ AB 사칙연산/소스.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main()
+{
+	int a, b;
+
+	scanf("%d %d", &a, &b);
+
+	printf("%d\n", a + b);
+	printf("%d\n", a - b);
+	printf("%d\n", a * b);
+	printf("%d\n", a / b);
+	printf("%d", a % b);
+
+	exit(0);
+
+}
